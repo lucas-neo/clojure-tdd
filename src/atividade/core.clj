4c3967ff1c0420625260a67d0ce6d238c931ab60
Lucas Neo <lucasneo@outlook.com.br>
2025-10-17 22:49:28 -0300

    refactor: apply refactor phase (TDD step 3)

--- src/atividade/core.clj
+++ src/atividade/core.clj
@@ -1,30 +1,52 @@
-(ns atividade.core)
+(ns atividade.core
+  (:require [clojure.string :as str]))
 
-(defn criar-tarefa [nome descricao]
-  (when (empty? nome)
-    (throw (IllegalArgumentException. "Nome não pode ser vazio")))
+(defn- validar-nome! [nome]
+  (when (str/blank? nome)
+    (throw (IllegalArgumentException. "Nome não pode ser vazio"))))
+
+(defn criar-tarefa
+  [nome descricao]
+  (validar-nome! nome)
   {:id 1
    :name nome
    :description descricao
    :status :em-andamento})
 
-(defn marcar-concluida [tarefa]
-    (assoc tarefa :status :concluida))
+(defn marcar-concluida
+  [tarefa]
+  (if (= :concluida (:status tarefa))
+    tarefa
+    (assoc tarefa :status :concluida)))
 
-(defn marcar-em-andamento [tarefa]
+(defn marcar-em-andamento
+  [tarefa]
   (when (= :concluida (:status tarefa))
     (throw (IllegalStateException. "Tarefa concluída não pode voltar para em andamento")))
-  (assoc tarefa :status :em-andamento))
+  (if (= :em-andamento (:status tarefa))
+    tarefa
+    (assoc tarefa :status :em-andamento)))
 
-(defn editar-tarefa [tarefa nome descricao]
-  (when (empty? nome)
-    (throw (IllegalArgumentException. "Nome não pode ser vazio")))
-  (assoc tarefa 
-    :name nome
-    :description descricao))
+(defn editar-tarefa
+  [tarefa nome descricao]
+  (validar-nome! nome)
+  (if (and (= nome (:name tarefa))
+           (= descricao (:description tarefa)))
+    tarefa
+    (assoc tarefa
+      :name nome
+      :description descricao)))
 
-(defn excluir-tarefa [tarefas id]
-  (when-not (some #(= id (:id %)) tarefas)
-    (throw (IllegalArgumentException. "Tarefa não encontrada")))
-  (remove #(= id (:id %)) tarefas))
-  
+(defn excluir-tarefa
+  [tarefas id]
+  (let [[acc-t encontrou?]
+        (reduce (fn [[acc ok?] t]
+                  (if (= id (:id t))
+                    [acc true]
+                    [(conj! acc t) ok?]))
+                [(transient []) false]
+                tarefas)]
+    (when-not encontrou?
+      (throw (IllegalArgumentException. "Tarefa não encontrada")))
+    (persistent! acc-t)))
+    
